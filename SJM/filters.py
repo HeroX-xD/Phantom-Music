d52c148c02ade30c7649fea0a4bf832ff3aef57b
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-11 14:55:31 +0530

    Update filters.py

--- SJM/filters.py
+++ SJM/filters.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from typing import List, Union
-from Config import COMMAND_PREFIXES
+from config import COMMAND_PREFIXES
 
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
