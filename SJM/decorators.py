b414974880c8ded4b1fb9fdbcd0333b8496c824c
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-11 14:54:57 +0530

    Update decorators.py

--- SJM/decorators.py
+++ SJM/decorators.py
@@ -1,7 +1,7 @@
 from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
-from Config import SUDO_USERS
+from config import SUDO_USERS
 from SJM.admins import get_administrators
 
 
