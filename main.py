8cc84b7983c27ccd85fefba8944e0a636614a7a5
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-11 09:30:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from Config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
-from pyrogram import Client
-from pytgcalls import PyTgCalls
+import asyncio
+from pytgcalls import idle
+from Trickyabhi.Abhishek import SUPPORT
+from Trickyabhi.main import call_py, bot, user as Herox
 
-bot = Client(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={"root": "Herox"},
-)
-
-user = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)
-
-call_py = PyTgCalls(user, overload_quiet_mode=True)
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+    await bot.join_chat("TrickyAbhii_Op")
+    await blaze.send_message(
+               SUPPORT,
+            "<b>Congrats!! Music Bot has started successfully!</b>",
+        )    
+                          
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
