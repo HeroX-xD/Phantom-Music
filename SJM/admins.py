a3d3df9bc03148f7b6ad9ca4a37170f708ae7fa7
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-11 12:08:00 +0530

    Update admins.py

--- SJM/admins.py
+++ SJM/admins.py
@@ -1,6 +1,6 @@
 from typing import List
 from pyrogram.types import Chat
-from Process.Cache.admins import get as gett, set
+from SJM.Cache.admins import get as gett, set
 
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
