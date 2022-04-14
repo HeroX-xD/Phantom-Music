38dbdbb1ab55f263c2db4bda005d791059ad7f8a
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-14 09:35:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # yooo guiz Herox 
 import os
+from Python_ARQ import ARQ
 from os import getenv
 from dotenv import load_dotenv
 
@@ -30,3 +31,5 @@ IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
 IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/92e8c83e9148c6fea5f3b.png")
 IMG_6 = getenv("IMG_6", "https://telegra.ph/file/92e8c83e9148c6fea5f3b.png")
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
+
