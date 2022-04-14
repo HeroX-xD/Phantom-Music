63209fba0b24639d47db17fa54ca73ebec266602
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-14 11:49:52 +0530

    Update broadcast.py

--- Herox/broadcast.py
+++ Herox/broadcast.py
@@ -8,33 +8,7 @@ from aiohttp import ClientSession
 from config import SUDO_USERS, BOT_TOKEN
 from pyrogram.errors import UserAlreadyParticipant
 
-from tricky import client as USER
 from config import SUDO_USERS
-
-@Client.on_message(filters.command(["broadcast"]))
-async def broadcast(_, message: Message):
-    sent=0
-    failed=0
-    if message.from_user.id not in SUDO_USERS:
-        return
-    else:
-        wtf = await message.reply("`Starting a broadcast...`")
-        if not message.reply_to_message:
-            await wtf.edit("Please Reply to a Message to broadcast!")
-            return
-        lmao = message.reply_to_message.text
-        async for dialog in USER.iter_dialogs():
-            try:
-                await USER.send_message(dialog.chat.id, lmao)
-                sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
-                await asyncio.sleep(3)
-            except:
-                failed=failed+1
-                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
-                
-            
-        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
 
 
 @Client.on_message(filters.command("banall") &
