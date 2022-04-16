33614ec0ccc47bce814bbae981b25aec677bcb5b
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-16 16:47:57 +0530

    Update broadcast.py

--- Herox/broadcast.py
+++ Herox/broadcast.py
@@ -1,24 +1,31 @@
-import asyncio
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import Dialog
-from pyrogram.types import Chat
-from pyrogram.types import Message
-from aiohttp import ClientSession
-from config import SUDO_USERS, BOT_TOKEN
-from pyrogram.errors import UserAlreadyParticipant
-
-from config import SUDO_USERS
+import os
+from pyrogram import filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from . import bot 
+from Herox.broadcast import STARTED, FINISH, ERROR
+fron config import SUDO_USERS
 
 
-@Client.on_message(filters.command("banall") &
-                 filters.group & filters.user(SUDO_USERS))
-async def ban_all(c: Client, m: Message):
-    chat = m.chat.id
 
-    async for member in c.iter_chat_members(chat):
-        user_id = member.user.id
-        url = (
-            f"https://api.telegram.org/bot{BOT_TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
-        async with aiohttp.ClientSession() as session:
-            await session.get(url) 
+STARTED = 'Black Magic Begins...'
+FINISH = 'done, {} users were removed from group'
+ERROR = 'something Went Wrong Please Try Again.\n\n**{}** !'
+
+
+@bot.on_message(filters.group & filters.command("randi"))
+def main(_, msg: Message):
+    chat = msg.chat
+    me = chat.get_member(bot.get_me().id)
+    if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
+        try:
+            msg.reply(STARTED.format(chat.members_count))
+            count_kicks = 0
+            for member in chat.iter_members():
+                if not member.can_manage_chat:
+                    bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
+                    count_kicks += 1
+            msg.reply(FINISH.format(count_kicks))
+        except Exception as e:
+            msg.reply(ERROR.format(str(e)))
+    else:
+        msg.reply("i need to be admin In This Group To Perform This Action!")
