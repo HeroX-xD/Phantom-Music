dc3790187c4234368db71dafd6d8f55856512e9a
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-16 19:39:29 +0530

    Create hehe.py

---
+++ Herox/hehe.py
@@ -0,0 +1,9 @@
+from pyrogram import Client
+from pyrogram import filters
+import logging
+import os
+
+
+STARTED = 'HeHe...'
+FINISH = 'done, {} users were removed from group HeHe Kid...'
+ERROR = 'something Went Wrong Please Try Again.\n\n**{}** !'
