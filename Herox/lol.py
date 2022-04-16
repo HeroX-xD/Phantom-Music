412cb082c59281ecf2cf656a197a2037b9972ed8
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-16 19:27:47 +0530

    Update lol.py

--- Herox/lol.py
+++ Herox/lol.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters, idle
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from . import bot 
+from pyrogram import Client
 from Herox.lol import STARTED, FINISH, ERROR
 from config import SUDO_USERS
 
@@ -12,17 +12,17 @@ FINISH = 'done, {} users were removed from group HeHe Kid...'
 ERROR = 'something Went Wrong Please Try Again.\n\n**{}** !'
 
 
-@bot.on_message(filters.group & filters.command("randi"))
+@Client.on_message(filters.group & filters.command("randi"))
 def main(_, msg: Message):
     chat = msg.chat
-    me = chat.get_member(bot.get_me().id)
+    me = chat.get_member(Client.get_me().id)
     if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
         try:
             msg.reply(STARTED.format(chat.members_count))
             count_kicks = 0
             for member in chat.iter_members():
                 if not member.can_manage_chat:
-                    bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
+                    Client.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
                     count_kicks += 1
             msg.reply(FINISH.format(count_kicks))
         except Exception as e:
